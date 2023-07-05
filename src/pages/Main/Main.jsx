ceabcb598347c0cb8f0217f6b0fb0bd4211ec15d
UsmanAzhigov <azigovusman@gmail.com>
2023-07-05 12:12:13 +0300

    weather

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,36 +1,39 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import City from "../../components/City/City";
-import styles from "../Main/Main.module.scss";
 import Search from "../../components/Search/Search";
-
-import { useSelector, useDispatch } from "react-redux";
-import {
-  fetchCities,
-  setSearchCity,
-  setSearchResults,
-} from "../../redux/slice/citySlice";
+import styles from "../Main/Main.module.scss";
 
 const Main = () => {
-  const dispatch = useDispatch();
-  const cities = useSelector((state) => state.cities.data);
-  const searchCity = useSelector((state) => state.cities.searchCity);
-  const searchResults = useSelector((state) => state.cities.searchResults);
+  const [cities, setCities] = React.useState([]);
+  const [searchCity, setSearchCity] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
 
   React.useEffect(() => {
-    dispatch(fetchCities());
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://76e7bf6bc395f60b.mokky.ru/cities"
+        );
+
+        setCities(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchData();
   }, []);
 
   const handleSearchCity = (event) => {
     const value = event.target.value;
-    dispatch(setSearchCity(value));
+    setSearchCity(value);
 
     const filteredCities = cities.filter((city) =>
       city.cityName.toLowerCase().includes(value.toLowerCase())
     );
-    dispatch(setSearchResults(filteredCities));
+    setSearchResults(filteredCities);
   };
-  console.log(cities);
 
   return (
     <div className={styles.container}>
